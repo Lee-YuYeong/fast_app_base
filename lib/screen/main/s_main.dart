e1fd3157f07db776ac369422a53f44c4750ebc8a
Lee-YuYeong <dbdud312066@gmail.com>
2024-08-27 13:23:34 +0900

    feat: 메인화면 탭바 구성 변경 및 파일 생성

--- lib/screen/main/s_main.dart
+++ lib/screen/main/s_main.dart
@@ -1,5 +1,7 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:fast_app_base/screen/main/tab/tab_item.dart';
 import 'package:fast_app_base/screen/main/tab/tab_navigator.dart';
+import 'package:fast_app_base/screen/main/write/d_write.dart';
 import 'package:flutter/material.dart';
 
 import '../../common/common.dart';
@@ -48,6 +50,12 @@ class MainScreenState extends State<MainScreen> with SingleTickerProviderStateMi
           ),
         ),
         bottomNavigationBar: _buildBottomNavigationBar(context),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            final result = await WriteTodoDialog().show();
+          },
+          child: const Icon(EvaIcons.plus),
+        ),
       ),
     );
   }
