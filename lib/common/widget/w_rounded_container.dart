47843414b1fce714a1ae30ab79a926fe532c019e
Lee-YuYeong <dbdud312066@gmail.com>
2024-08-27 13:22:53 +0900

    feat: 컨테이너 커스텀 위젯 생성

---
+++ lib/common/widget/w_rounded_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+class RoundedContainer extends StatelessWidget {
+
+  final Widget child;
+  final EdgeInsets? padding;
+  final EdgeInsets? margin;
+  final Color? color;
+  final double radiusValue;
+  final BorderRadiusGeometry? radius;
+
+  const RoundedContainer({super.key, required this.child, this.padding, this.margin, this.color, this.radiusValue = 10, this.radius});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: padding,
+      margin: margin,
+      decoration: BoxDecoration(
+        borderRadius: radius ?? BorderRadius.circular(radiusValue),
+        color: color,
+      ),
+      child: child,
+    );
+  }
+}
